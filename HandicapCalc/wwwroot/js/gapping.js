301840be6b18e9b412a5080cfe624aa9e948aa62
Micah Brown <brown_micah@outlook.com>
2024-03-15 22:49:15 -0600

    updating March 15

--- HandicapCalc/wwwroot/js/gapping.js
+++ HandicapCalc/wwwroot/js/gapping.js
@@ -1,31 +1,40 @@
 ﻿function collectClubData() {
-    // create an empty object to store club data
-    const clubData = {};
+    // create an empty array to store club data
+    const clubArray = [];
+    alert("hi");
     // iterate through each club div and collect input values
     document.querySelectorAll('.club').forEach(function (clubDiv) {
         const clubName = clubDiv.querySelector('label').textContent; //remove white space on inputs? validation?
         const clubYardage = clubDiv.querySelector('input').value;
+        const clubId = clubDiv.querySelector('input').id;
+        
         // Add club data to the object
-        clubData[clubName] = clubYardage;
+        const clubObject = {
+            club: clubName,
+            yardage: clubYardage,
+            id: clubId
+        };
+
+        clubArray.push(clubObject);
     });
 
     // pass the club data object to the analyzeGaps() function
-    console.log(clubData);
-    analyzeGaps(clubData);
+    console.log(clubArray[2].id);
+    analyzeGaps(clubArray);
 }
 
 function analyzeGaps(cd) {
-    const clubData = { cd };
+    const orderedArray = cd.sort((a, b) => a.id - b.id);
+    console.log(orderedArray);
+    const gaps = {};
 
-    var gaps = [];
+    for (i = 0; i < orderedArray.length; i++) {
 
-    for (i = 0; i < clubData.length; i++) {
-
-        const upperClub = clubData[i].clubYardage.value;
-        const lowerClub = clubdata[i].clubYardage.value;
+        const upperClub = orderedArray[i].clubYardage.value;
+        const lowerClub = orderedArray[i+1].clubYardage.value;
 
         const gap = upperClub - lowerClub;
-        gaps.push(gap);
+        gaps.push({ name: orderedArray.clubName.toString(), gap: gap });
     }
 
     console.log(gaps);
