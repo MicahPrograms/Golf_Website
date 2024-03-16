f7cc73e128c4b911b3ee2dd97a66e8d56d8c80b1
Micah Brown <brown_micah@outlook.com>
2024-03-16 00:32:48 -0600

    final commit for March 15 - main functionality complete

--- HandicapCalc/wwwroot/js/gapping.js
+++ HandicapCalc/wwwroot/js/gapping.js
@@ -1,13 +1,13 @@
 ﻿function collectClubData() {
     // create an empty array to store club data
     const clubArray = [];
-    alert("hi");
     // iterate through each club div and collect input values
     document.querySelectorAll('.club').forEach(function (clubDiv) {
+        
         const clubName = clubDiv.querySelector('label').textContent; //remove white space on inputs? validation?
         const clubYardage = clubDiv.querySelector('input').value;
         const clubId = clubDiv.querySelector('input').id;
-        
+
         // Add club data to the object
         const clubObject = {
             club: clubName,
@@ -15,27 +15,38 @@
             id: clubId
         };
 
+        if (clubYardage == "" || clubYardage == null) {
+            clubDiv.remove();
+        };
+
         clubArray.push(clubObject);
     });
-
     // pass the club data object to the analyzeGaps() function
-    console.log(clubArray[2].id);
-    analyzeGaps(clubArray);
+    analyzeGaps(clubArray);    
 }
 
 function analyzeGaps(cd) {
-    const orderedArray = cd.sort((a, b) => a.id - b.id);
+    const cleanArray = cd.filter(club => club.yardage !== "");
+    const orderedArray = cleanArray.sort((a, b) => a.id - b.id);
     console.log(orderedArray);
-    const gaps = {};
+    const gaps = [];
 
-    for (i = 0; i < orderedArray.length; i++) {
+    for (let i = 0; i < orderedArray.length - 1; i++) {
 
-        const upperClub = orderedArray[i].clubYardage.value;
-        const lowerClub = orderedArray[i+1].clubYardage.value;
+        const upperClub = orderedArray[i].yardage;
+        const lowerClub = orderedArray[i + 1].yardage;
 
         const gap = upperClub - lowerClub;
-        gaps.push({ name: orderedArray.clubName.toString(), gap: gap });
+        gaps.push({ name: orderedArray[i].club, gap: gap });
     }
-
     console.log(gaps);
+
+    document.querySelectorAll('h4').forEach(function (h4, index) {
+            h4.textContent = gaps[index].gap;
+    });
 }
+
+function resetGappingData() {
+    location.reload();
+}
+
